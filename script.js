c9f25a9e228dbe55a67e3e8eae663e81fab142e1
THDWarstrike <89410413+THDWarstrike@users.noreply.github.com>
2022-03-03 08:02:48 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+secretMessage.pop(09-003)
+console.log(secretMessage.length);
+secretMessage.push('to', 'program')
+secretMessage['easily'] = 'right'
+secretMessage.shift()
+secretMessage.unshift('Programming')
+secretMessage.pop(6, 7, 8, 9, 00)
+console.log(secretMessage.join(''))
